d9cf2ad0298c66ce9eb31e005403fe7ca0d61c00
HajinKimm <11rlagkwls@naver.com>
2023-07-03 03:27:43 +0900

    반응형웹 tablet, mobile 작업

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,34 @@
 var swiper = new Swiper(".myscrubsCollection", {
-    slidesPerView: 4.5,
-    spaceBetween: 60,
     freeMode: true,
+    breakpoints: {
 
+      0: {
+          slidesPerView: 2, //화면에 보이는 이미지 갯수
+          spaceBetween: 30, //이미지사이의 여백
+      },
+      768: {
+          slidesPerView: 2, //화면에 보이는 이미지 갯수
+          spaceBetween: 30, //이미지사이의 여백
+      },
+      900: {
+          slidesPerView: 2.5, //화면에 보이는 이미지 갯수
+          spaceBetween: 30, //이미지사이의 여백
+      },
+      1025: {
+          slidesPerView: 3, //화면에 보이는 이미지 갯수
+          spaceBetween: 30, //이미지사이의 여백
+      },
+      1210: {
+          slidesPerView: 3.5,
+          spaceBetween: 30,
+      },
+      1400: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+      },
+      1580: {
+          slidesPerView: 4.5,
+          spaceBetween: 30,
+      },
+    },
   });
